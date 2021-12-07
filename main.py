10e86115f4aee5e87f7f96678db8a7c62a1d5921
NickNaPozitive <korbofos.nn@gmail.com>
2021-12-07 18:10:05 +0300

    new def's

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 from prettytable import PrettyTable as pt
 import psutil as pu
+from pprint import pprint as pp
 import datetime
 from collections import namedtuple as nt
 
 
-# pprint(datetime.datetime.fromtimestamp(pu.boot_time()).strftime("%Y-%m-%d %H:%M:%S"))
-
-
 def get_cpu():
-    counter_cpu = pu.cpu_count(logical=True)
-    return counter_cpu
-
-
-def cpu_freq():
-    info = []
-    data = pu.cpu_freq(percpu=True)
-    for i in data:
-        info.append(i[0])
-    return info
-
-
-def cpu_freq_current():
+    dict = {}
+    j = 1
     current = 0
-    data = pu.cpu_freq(percpu=True)
-    for i in data:
+
+    counter_cpu = pu.cpu_count(logical=True)
+    data_freq = pu.cpu_freq(percpu=True)
+    dict.update({
+        'cores': counter_cpu
+    })
+    for i in data_freq:
+        dict.update({
+            f'core {j}': i[0]
+        })
+        j += 1
+    for i in data_freq:
         current += i[0]
     result = current / 12
-    return result
+    dict.update({
+        'freq_current': round(result, 4)
+    })
+    return dict
 
 
 def disk_usage():
@@ -43,22 +43,41 @@ def disk_usage():
     return dict
 
 
+def network_info():
+    dict = {}
+    data = pu.net_io_counters()
+    data._asdict()
+    dict.update(
+        {'bytes_sent': f"{round((data.bytes_sent / 1000000000), 3)} Gb",
+         'bytes_recv': f"{round((data.bytes_recv / 1000000000), 3)} Gb",
+         }
+    )
+
+    return dict
+
+
 def show(**kwargs):
     i = 1
     my_table = pt()
     my_table.field_names = ["TYPE", "INFO"]
-    my_table.add_row(["cores", kwargs['cpu_num']])
-    for key in kwargs["cpu_freq"]:
-        my_table.add_row([f"freq {i} core", key])
-        i += 1
+    for key, value in kwargs["get_cpu"].items():
+        if key == 'cores':
+            my_table.add_row(["cores", value])
+        elif key == 'freq_current':
+            my_table.add_row(["freq_current", value])
+        else:
+            my_table.add_row([f"freq {i} core", value])
+            i += 1
     for key in kwargs["disk_usage"]:
         my_table.add_row([key, kwargs["disk_usage"][key]])
+    for key in kwargs["network_info"]:
+        my_table.add_row([key, kwargs["network_info"][key]])
 
     print(my_table)
 
 
 def run():
-    show(cpu_num=get_cpu(), cpu_freq=cpu_freq(), disk_usage=disk_usage())
+    show(get_cpu=get_cpu(), disk_usage=disk_usage(), network_info=network_info())
 
 
 if __name__ == '__main__':
